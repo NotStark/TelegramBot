c77d52a8da2d298becb04779385cfc2bc8b471b3
notstark <nottonyonlystark@gmail.com>
2023-07-04 22:40:56 +0530

    k

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,17 +1,19 @@
 import os
 from typing import List
 import yaml
-from collections import namedtuple
+from dotmap import DotMap
 
 
 languages = {}
 commands = {}
+
 
 def get_command(value: str) -> List:
     return commands["command"][value]
 
 def get_string(lang: str):
     return languages[lang]
+
 
 def load_commands():
     for filename in os.listdir(r"./strings"):
@@ -23,18 +25,19 @@ def load_commands():
 
 def load_langs():
     if "en" not in languages:
-        languages["en"] = yaml.safe_load(
+        languages["en"] = DotMap(yaml.safe_load(
             open(r"./strings/langs/en.yml", encoding="utf8")
-        )
+        ))
+
 
     for filename in os.listdir(r"./strings/langs/"):
         if filename.endswith(".yml"):
             language_name = filename[:-4]
             if language_name == "en":
                 continue
-            languages[language_name] = yaml.safe_load(
+            languages[language_name] = DotMap(yaml.safe_load(
                 open(r"./strings/langs/" + filename, encoding="utf8")
-            )
+            ))
             for item in languages["en"]:
                 if item not in languages[language_name]:
                     languages[language_name][item] = languages["en"][item]

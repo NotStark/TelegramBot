24c50165a14b7e47f9e2cdb93ef91d6062c12501
notstark <nottonyonlystark@gmail.com>
2023-07-06 10:12:14 +0530

    sx

--- TeleBot/core/chat_status.py
+++ TeleBot/core/chat_status.py
@@ -17,12 +17,12 @@ async def is_bot_admin(chat_id: int, permission: Any = None) -> bool:
 
 async def is_user_admin(chat_id: int, user_id: int, permission: Any = None) -> bool:
     if await is_invincible(user_id) or user_id == chat_id:
-        print("tru h")
         return True
     elif permission is None and user_id in await get_admins(chat_id):
+        
         return True
     else:
-        chat_member = await app.get_chat_member(chat_id, BOT_ID)
+        chat_member = await app.get_chat_member(chat_id, user_id)
         privileges = chat_member.privileges.__dict__ if chat_member.privileges is not None else {}
         return permission in privileges and privileges[permission]
 

19acbd57eb4a0a567886d93100b03e37d4cd9f2d
notstark <nottonyonlystark@gmail.com>
2023-07-06 16:04:48 +0530

    k

--- TeleBot/core/custom_filter.py
+++ TeleBot/core/custom_filter.py
@@ -2,7 +2,7 @@ import re
 from config import HANDLERS
 from typing import Union, List
 from pyrogram import  filters
-from TeleBot.helpers.functions import disable_action
+from TeleBot.core.functions import disable_action
 
 
 def command(commands: Union[str, List[str]], prefixes: Union[str, List[str]] = HANDLERS, disable: bool = True):

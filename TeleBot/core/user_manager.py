442d9bee4bd967ff0c5ab34f5a1c2f275362d580
notstark <nottonyonlystark@gmail.com>
2023-07-07 20:29:28 +0530

    sx

--- TeleBot/core/user_manager.py
+++ TeleBot/core/user_manager.py
@@ -1,10 +1,11 @@
 from TeleBot import BOT_ID, app
 from typing import Any
-from .functions import is_invincible as x , get_admins as y 
+from TeleBot.core import functions
 
+get_admins = functions.get_admins
 
 async def is_bot_admin(chat_id: int, permission: Any = None) -> bool:
-    if permission is None and BOT_ID in await y(chat_id):
+    if permission is None and BOT_ID in await get_admins(chat_id):
         return True
     else:
         chat_member = await app.get_chat_member(chat_id, BOT_ID)
@@ -17,10 +18,10 @@ async def is_bot_admin(chat_id: int, permission: Any = None) -> bool:
 
 
 async def is_user_admin(chat_id: int, user_id: int, permission: Any = None) -> bool:
-    
-    if await x(user_id) or user_id == chat_id:
+    is_invincible = functions.is_invincible
+    if await is_invincible(user_id) or user_id == chat_id:
         return True
-    elif permission is None and user_id in await y(chat_id):
+    elif permission is None and user_id in await get_admins(chat_id):
         return True
     else:
         chat_member = await app.get_chat_member(chat_id, user_id)
@@ -31,4 +32,3 @@ async def is_user_admin(chat_id: int, user_id: int, permission: Any = None) -> b
         )
         return permission in privileges and privileges[permission]
     
-

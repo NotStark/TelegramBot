e2357a871f18aa6b2219ed376ebf5be066d29448
notstark <nottonyonlystark@gmail.com>
2023-07-07 07:02:21 +0530

    hmm

--- TeleBot/mongo/approve_db.py
+++ TeleBot/mongo/approve_db.py
@@ -2,36 +2,33 @@ from TeleBot.mongo import db
 
 approvedb = db.approve
 
-async def approve_user(chat_id : int,user_id : int ):
-    chat = await approvedb.find_one({"chat_id" : chat_id})
+
+async def approve_user(chat_id: int, user_id: int):
+    await approvedb.update_one(
+        {"chat_id": chat_id},
+        {"$addToSet": {"user_ids": user_id}},
+        upsert=True
+    )
+
+
+async def is_approved(chat_id: int, user_id: int) -> bool:
+    chat = await approvedb.find_one({"chat_id": chat_id})
     if chat:
-        list = chat.get("user_ids")
-        list.append(user_id)
-        return await approvedb.update_one({"chat_id": chat_id}, {"$set": {"user_ids": list}}, upsert=True)
-    list = [user_id]
-    return await approvedb.update_one({"chat_id" : chat_id},{"$set" : {"user_ids" : list}}, upsert=True)   
+        return user_id in chat.get("user_ids", [])
+    return False
 
 
-async def isApproved(chat_id : int,user_id : int) -> bool:
-    chat = await approvedb.find_one({"chat_id" : chat_id})
-    if chat:
-        check = chat.get("user_ids")
-        if user_id in check:
-            return True
-        return False
+async def disapprove_user(chat_id: int, user_id: int):
+    await approvedb.update_one(
+        {"chat_id": chat_id},
+        {"$pull": {"user_ids": user_id}}
+    )
 
-async def disapprove_user(chat_id : int,user_id : int):
-    chat = await approvedb.find_one({"chat_id" : chat_id}) 
-    if chat:
-        list = chat.get("user_ids")
-        list.remove(user_id)
-        return await approvedb.update_one({"chat_id": chat_id}, {"$set": {"user_ids": list}}, upsert=True) 
 
-async def disapprove_all(chat_id : int):
-    await approvedb.delete_one({"chat_id" : chat_id}) 
-    
-async def approved_users(chat_id : int) -> list:
-    chat = await approvedb.find_one({"chat_id" : chat_id}) 
-    if chat :        
-        return chat["user_ids"]
-    return []
+async def disapprove_all(chat_id: int):
+    await approvedb.delete_one({"chat_id": chat_id})
+
+
+async def approved_users(chat_id: int) -> list:
+    chat = await approvedb.find_one({"chat_id": chat_id})
+    return chat.get("user_ids", []) if chat else []

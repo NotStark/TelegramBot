a22a8bae34e1f27ec61c0a0421346febba3cd3eb
notstark <nottonyonlystark@gmail.com>
2023-07-07 15:17:32 +0530

    sx

--- TeleBot/core/decorators/chat_status.py
+++ TeleBot/core/decorators/chat_status.py
@@ -1,39 +1,11 @@
 from typing import Any
 from pyrogram.types import Message
-from TeleBot import app, BOT_ID
 from pyrogram.enums import ChatType
 from functools import wraps
-from ..functions import get_admins, is_invincible
+from ..user_manager import is_bot_admin , is_user_admin
 from .lang import get_chat_lang
 from ..functions import remove_markdown, handle_exception
 
-
-async def is_bot_admin(chat_id: int, permission: Any = None) -> bool:
-    if permission is None and BOT_ID in await get_admins(chat_id):
-        return True
-    else:
-        chat_member = await app.get_chat_member(chat_id, BOT_ID)
-        privileges = (
-            chat_member.privileges.__dict__
-            if chat_member.privileges is not None
-            else {}
-        )
-        return permission in privileges and privileges[permission]
-
-
-async def is_user_admin(chat_id: int, user_id: int, permission: Any = None) -> bool:
-    if await is_invincible(user_id) or user_id == chat_id:
-        return True
-    elif permission is None and user_id in await get_admins(chat_id):
-        return True
-    else:
-        chat_member = await app.get_chat_member(chat_id, user_id)
-        privileges = (
-            chat_member.privileges.__dict__
-            if chat_member.privileges is not None
-            else {}
-        )
-        return permission in privileges and privileges[permission]
 
 
 def admins_stuff(permission: Any = None, bot: bool = False):
---
+++ TeleBot/core/user_manager.py
@@ -0,0 +1,30 @@
+from TeleBot import app, BOT_ID
+from typing import Any
+from .functions import get_admins, is_invincible
+
+async def is_bot_admin(chat_id: int, permission: Any = None) -> bool:
+    if permission is None and BOT_ID in await get_admins(chat_id):
+        return True
+    else:
+        chat_member = await app.get_chat_member(chat_id, BOT_ID)
+        privileges = (
+            chat_member.privileges.__dict__
+            if chat_member.privileges is not None
+            else {}
+        )
+        return permission in privileges and privileges[permission]
+
+
+async def is_user_admin(chat_id: int, user_id: int, permission: Any = None) -> bool:
+    if await is_invincible(user_id) or user_id == chat_id:
+        return True
+    elif permission is None and user_id in await get_admins(chat_id):
+        return True
+    else:
+        chat_member = await app.get_chat_member(chat_id, user_id)
+        privileges = (
+            chat_member.privileges.__dict__
+            if chat_member.privileges is not None
+            else {}
+        )
+        return permission in privileges and privileges[permission]

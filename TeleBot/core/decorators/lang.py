bf9216eff0f8974482849cb3765f9d26ee6ec28b
notstark <nottonyonlystark@gmail.com>
2023-07-07 19:25:47 +0530

    sx

--- TeleBot/core/decorators/lang.py
+++ TeleBot/core/decorators/lang.py
@@ -2,7 +2,7 @@ from strings import get_string
 from pyrogram.types import Message
 from functools import wraps
 from TeleBot.mongo.lang_db import get_lang
-from TeleBot.core.functions import handle_exception
+from .chat_status import handle_exception
 
 
 def language(func):

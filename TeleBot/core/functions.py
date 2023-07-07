73837cb7c37c4ad3145af7ab967b3ea046337ea1
notstark <nottonyonlystark@gmail.com>
2023-07-07 15:10:52 +0530

    less see

--- TeleBot/core/functions.py
+++ TeleBot/core/functions.py
@@ -4,10 +4,7 @@ from TeleBot import app
 from typing import Callable, Union
 from pyrogram import Client
 from TeleBot.mongo.disable_db import get_disabled_commands, get_disable_delete
-from cachetools import TTLCache
-from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import ChatWriteForbidden, MessageDeleteForbidden
-from time import perf_counter
 from pyrogram import enums, types
 from TeleBot.core.decorators.chat_status import is_user_admin
 from TeleBot.mongo.connection_db import get_connected_chat,is_connection_allowed, disconnect_chat
@@ -18,14 +15,7 @@ async def is_invincible(user_id : int) -> bool:
   return user_id in INVINCIBLES
 
 
-admin_cache = TTLCache(maxsize=610, ttl=60*10,timer=perf_counter)  
 
-async def get_admins(chat_id : int):
-    if chat_id in admin_cache:
-        return admin_cache[chat_id]
-    admins = [member.user.id async for member in app.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS) ]  
-    admin_cache[chat_id] =  admins
-    return admins
 
 
 async def get_readable_time(seconds: int) -> str:

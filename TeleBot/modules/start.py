db6b40bd48c4c7729831dcb4bb3116f4ff452963
notstark <nottonyonlystark@gmail.com>
2023-07-09 16:09:32 +0530

    sx

--- TeleBot/modules/start.py
+++ TeleBot/modules/start.py
@@ -33,8 +33,10 @@ async def _start(client, message, lang):
                     return await client.send_message(chat_id,lang.rules10)
                 chat = await client.get_chat(chat_idd)
                 try:
-                    txt, button = await button_markdown_parser(rules)       
+                    txt, button = await button_markdown_parser(rules)      
+                    txt , button 
                     await client.send_message(chat_id,lang.rules9.format(chat.title,txt),reply_markup = InlineKeyboardMarkup(button))
+                    return
                 except Exception as e: 
                     await client.send_message(chat_id,lang.rules9.format(chat.title,rules))
                     raise e

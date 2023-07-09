79f535f110ff32ae9bdda75b33a7cd63fb0e426d
notstark <nottonyonlystark@gmail.com>
2023-07-09 16:52:40 +0530

    sx

--- TeleBot/__main__.py
+++ TeleBot/__main__.py
@@ -101,7 +101,7 @@ async def main():
 @app.on_message(command(START_COMMAND))
 @language
 async def _start(client, message, lang):
-    print(HELPABLE)
+    print(HELPABLE["𝚁ᴜʟᴇꜱ"].__mod_name__)
     uptime = await get_readable_time((time.time() - StartTime))
     chat_id = message.chat.id
     args = message.text.split()
